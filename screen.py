5503a5cdb0de8999db5326a3818f7369c9cdeb25
Felipe Ferraz <felipeferraz1398@gmail.com>
2024-06-13 14:45:32 -0300

    add class View and Screen menu was visually improved

--- screen.py
+++ screen.py
@@ -1,6 +1,7 @@
 from services import cart
 from services.products import shampoo, lipstick, perfume
 from screenexceptions import *
+from view import *
 import time
 
 class Screen():
@@ -66,35 +67,41 @@ class Screen():
          # Lista para armazenar itens do carrinho (produto e gênero) para identificação de filtros pre definidos e localizar o produto
         self._list_cart = []  # Exemplo: [{'product': <produto>, 'genere': <gênero>}]
 
+        self._view = View()
+
 
     # Método para exibir o menu de produtos e capturar a escolha do usuário
     def _product_menu(self) -> None:
+        itens = ('Shampoo', 'Perfume', 'Batom')
         """
         Exibe o menu de seleção de produtos e gerencia a escolha do usuário.
         """
 
         while True:
             try:
-                print("""Selecione uma das opções
-                    1 - Shampoo
-                    2 - Perfume
-                    3 - Batom
-                    """)
-                product_choice = int(input("Digite aqui sua opção: "))
+                self._view.cabecalho('\033[34mPRODUTO\033[m')
+                c = 1
+                for item in itens:
+                    print(f'''\033[32m{c} - \033[34m{item}\033[m''')
+                    c += 1
+                print(self._view.linha())
+                product_choice = int(input("\033[33mDigite sua opção: \033[m"))
+                os.system("cls")
 
                 # Verifica se a escolha é válida
                 if product_choice not in [1, 2, 3]:
-                    raise InvalidChoice("Opção inválida! Tente novamente!")
+                    raise InvalidChoice("\033[31mOpção inválida! Tente novamente!\033[m")
 
                 # Chama o menu de gênero com base na escolha do produto
                 self._genere_menu(product_choice)
 
                 return
             except (InvalidChoice, ValueError):
-                raise InvalidChoice("Opção inválida! Tente novamente!")
+                print("\033[31mOpção inválida! Tente novamente!\033[m")
 
     # Método para exibir o menu de gêneros e capturar a escolha do usuário
     def _genere_menu(self, product: int) -> None:
+        opcoes = ('Masculino', 'Feminino', 'Infantil')
         """
         Exibe o menu de seleção de gênero do produto e gerencia a escolha do usuário.
         
@@ -105,29 +112,36 @@ class Screen():
         while True:
             try:
                 if product == 1:
-                    print("""Selecione uma das opções
-                        1 - Masculino
-                        2 - Feminino
-                        """)
+                    os.system("cls")
+                    self._view.cabecalho('\033[34mSELECIONE O GENÊRO\033[m')
+                    c = 1
+                    for item in opcoes:
+                        print(f'\033[32m{c} - \033[34m{item}\033[m')
+                        c += 1
                     self._save_product = self._shampoo
-                    genere_choice = int(input("Digite aqui sua opção: "))
-                    if genere_choice not in [1, 2]:
-                        raise InvalidChoice("Opção inválida! Tente novamente!")
+                    print(self._view.linha())
+                    genere_choice = int(input("\033[33mDigite sua opção: \033[m"))
+                    os.system("cls")
+                    if genere_choice not in [1,2,3]: # array com opções válidas
+                        raise InvalidChoice("\033[31mOpção inválida! Tente novamente!\033[m")
 
                 elif product == 2:
-                    print("""Selecione uma das opções
-                        1 - Masculino
-                        2 - Feminino
-                        3 - Infantil
-                        """)
+                    os.system("cls")
+                    self._view.cabecalho('\033[34mSELECIONE A OPÇÃO\033[m')
+                    c = 1
+                    for item in opcoes:
+                        print(f'\033[32m{c} - \033[34m{item}\033[m')
+                        c += 1
                     self._save_product = self._perfume
-                    genere_choice = int(input("Digite aqui sua opção: "))
-                    if genere_choice not in [1, 2, 3]:
-                        raise InvalidChoice("Opção inválida! Tente novamente!")
+                    print(self._view.linha())
+                    genere_choice = int(input("\033[33mDigite sua opção: \033[m"))
+                    os.system("cls")
+                    if genere_choice not in [1,2,3]: # array com opções válidas
+                        raise InvalidChoice("\033[31mOpção inválida! Tente novamente!\033[m")
 
                 elif product == 3:
                     self._save_product = self._lipstick
-                    genere_choice = 0  # Batom é unissex
+                    genere_choice = 0
 
                 # Processa a escolha do gênero
                 self._handle_choice(genere_choice)
@@ -153,17 +167,18 @@ class Screen():
         action = self._save_product(genere)
         list_products = action.all_products()
         index = 0
-        print("Escolha um produto:")
+        self._view.cabecalho("\033[34mEscolha um produto\033[m")
         for product in list_products:
             index += 1
-            print(f"{index} - {product}")
+            print(f'''\033[32m{index} - \033[34m{product}\033[m''')
         try:
-            choice = int(input("Digite uma opção: "))
+            print(self._view.linha())
+            choice = int(input("\033[33mDigite aqui sua opção: \033[m"))
             if choice <= index and choice > 0:
                 return list_products, choice - 1  # retornando a lista e a escolha -1 (array comeca em 0)
-            raise InvalidChoice("Opção inválida!")
+            raise InvalidChoice("\033[31mOpção inválida! Tente novamente!\033[m")
         except (InvalidChoice, ValueError):
-            raise InvalidChoice("Opção inválida!")
+            raise InvalidChoice("\033[31mOpção inválida! Tente novamente!\033[m")
 
     # Método para exibir os detalhes dos produtos
     def _print_products_details(self, genere: str) -> list:
@@ -193,16 +208,20 @@ class Screen():
         action = self._save_product(genere)
         products, choice = self._print_products(genere)
         products_details = self._print_products_details(genere)
-        print(f"Detalhes do produto {products[choice]}")
+        os.system("cls")
+        self._view.cabecalho(f"\033[34mDetalhes do produto {products[choice]}\033[m")
         print(products_details[choice])
         while True:
             try:
-                add_quantity = int(input("Digite a quantidade que deseja adicionar: "))
+                print(self._view.linha())
+                add_quantity = int(input("\033[33mDigite a quantidade que deseja adicionar: \033[m"))
                 validate = action.increase_quantity(products[choice], add_quantity)
                 if validate:
-                    print("Quantidade adicionada!")
+                    print("\033[32mQuantidade adicionada!\033[m")
                     products_details = self._print_products_details(genere)
-                    print(f"Novo total: {products_details[choice]}")
+                    self._view.cabecalho(f"\033[34mNovo total: {products_details[choice]}\033[m")
+                    sleep(2)
+                    os.system("cls")
                     return
             except:
                 raise ValueError("Valor inválido, digite um valor válido!")
@@ -225,13 +244,15 @@ class Screen():
         action = self._save_product(genere)  # Instancia o objeto do produto com base no gênero
         products, choice = self._print_products(genere)  # Exibe os produtos e obtém a escolha do usuário
         products_details = self._print_products_details(genere)  # Obtém os detalhes dos produtos
-        print(f"Detalhes do produto {products[choice]}")  # Mostra os detalhes do produto escolhido
+        os.system("cls")
+        self._view.cabecalho(f"\033[34mDetalhes do produto {products[choice]}\033[m")  # Mostra os detalhes do produto escolhido
         print(products_details[choice])
 
         while True:
             try:
                 cart = self._cart  # Obtém o objeto carrinho
-                quantity = int(input("Digite a quantidade que deseja do produto: "))  # Solicita a quantidade desejada
+                print(self._view.linha())
+                quantity = int(input("\033[33mDigite a quantidade que deseja do produto: \033[m"))  # Solicita a quantidade desejada
                 price = action.show_price(products[choice])  # Obtém o preço do produto
                 cart.add_item(products[choice], quantity, price)  # Adiciona o item ao carrinho
                 self._list_cart.append({'product': self._save_product,
@@ -253,16 +274,20 @@ class Screen():
         action = self._save_product(genere)
         products, choice = self._print_products(genere)
         products_details = self._print_products_details(genere)
-        print(f"Detalhes do produto {products[choice]}")
+        os.system("cls")
+        self._view.cabecalho(f"\033[34mDetalhes do produto {products[choice]}\033[m")
         print(products_details[choice])
         while True:
             try:
-                decrease_quantity = int(input("Digite a quantidade que deseja retirar: "))
+                print(self._view.linha())
+                decrease_quantity = int(input("\033[33mDigite a quantidade que deseja retirar: \033[m"))
                 validate = action.decrease_quantity(products[choice], decrease_quantity)
                 if validate:
-                    print("Quantidade retirada!")
+                    print("\033[32mQuantidade retirada!\033[m")
                     products_details = self._print_products_details(genere)
                     print(f"Novo total: {products_details[choice]}")
+                    sleep(2)
+                    os.system("cls")
                     return
             except:
                     raise ValueError("Valor inválido, digite um valor válido!")
@@ -279,14 +304,14 @@ class Screen():
         action = self._save_product(genere)
         while True:
             try:
-                product = input("Digite o produto: ")
-                quantity = int(input("Digite a quantidade: "))
-                price = float(input("Digite o preço: "))
+                product = input("\033[33mDigite o produto: \033[m")
+                quantity = int(input("\033[33mDigite a quantidade: \033[m"))
+                price = float(input("\033[33mDigite o preço: \033[m"))
                 validate = action.add_product(product, quantity, price)
                 if validate:
-                    print("Produto adicionado!")
+                    print("\033[32mProduto adicionado!\033[m")
                     products = self._print_products_details(genere)
-                    print(f"Novo produto: {products[-1]}")
+                    print(f"\033[34mNovo produto: \033[m{products[-1]}")
                     return
             except:
                 raise ValueError("Valor inválido, digite um valor válido!")
@@ -303,19 +328,22 @@ class Screen():
         action = self._save_product(genere)
         products, choice = self._print_products(genere)
         products_details = self._print_products_details(genere)
-        print(f"Detalhes do produto {products[choice]}")
+        os.system("cls")
+        self._view.cabecalho(f"\033[34mDetalhes do produto {products[choice]}\033[m")
         print(products_details[choice])
         while True:
             try:
-                price = float(input("Digite o novo preço do produto: "))
+                price = float(input("\033[33mDigite o novo preço do produto: \033[m"))
                 validate = action.edit_price(products[choice], price)
                 if validate:
-                    print("Preço alterado!")
+                    print("\033[32mPreço alterado!\033[m")
                     products_details = self._print_products_details(genere)
-                    print(f"Novo preço: {products_details[choice]}")
+                    self._view.cabecalho(f"\033[32mNovo preço: {products_details[choice]}\033[m")
+                    sleep(2)
+                    os.system("cls")
                     return
             except:
-                raise ValueError("Valor inválido, digite um valor válido!")
+                raise ValueError("\033[31mValor inválido, digite um valor válido!\033[m")
 
     # Método para lidar com operações CRUD com base na escolha inicial
     def _handle_crud(self, genere: str) -> None:
@@ -386,34 +414,34 @@ class Screen():
         # Verifica shampoo em falta
         shampoo_zero = self._shampoo(generes[1]).check_zero_quantity()
         if shampoo_zero:
-            validates.append(f'Shampoo {generes[1]} em falta:')
+            validates.append(f'\033[33mShampoo {generes[1]} em falta:\033[m')
             validates.append(shampoo_zero)
 
         shampoo_zero = self._shampoo(generes[2]).check_zero_quantity()
         if shampoo_zero:
-            validates.append(f'Shampoo {generes[2]} em falta:')
+            validates.append(f'\033[33mShampoo {generes[2]} em falta:\033[m')
             validates.append(shampoo_zero)
 
         # Verifica batom em falta
         lipstick_zero = self._lipstick(generes[0]).check_zero_quantity()
         if lipstick_zero:
-            validates.append('Batom em falta:')
+            validates.append('\033[33mBatom em falta:\033[m')
             validates.append(lipstick_zero)
 
         # Verifica perfume em falta
         perfume_zero = self._perfume(generes[1]).check_zero_quantity()
         if perfume_zero:
-            validates.append(f'Perfume {generes[1]} em falta:')
+            validates.append(f'\033[33mPerfume {generes[1]} em falta:\033[m')
             validates.append(perfume_zero)
 
         perfume_zero = self._perfume(generes[2]).check_zero_quantity()
         if perfume_zero:
-            validates.append(f'Perfume {generes[2]} em falta:')
+            validates.append(f'\033[33mPerfume {generes[2]} em falta:\033[m')
             validates.append(perfume_zero)
 
         perfume_zero = self._perfume(generes[3]).check_zero_quantity()
         if perfume_zero:
-            validates.append(f'Perfume {generes[3]} em falta:')
+            validates.append(f'\033[33mPerfume {generes[3]} em falta:\033[m')
             validates.append(perfume_zero)
 
         return validates
@@ -439,22 +467,27 @@ class Screen():
         """
         while True:
             try:
+                os.system("cls")
                 action = self._cart  # Obtém o objeto carrinho
-                print("Você está no carrinho!")
+                self._view.cabecalho('\033[34mCARRINHO DE COMPRAS\033[m')
                 self._product_menu()  # Executa a progressão de menus para selecionar o produto
                 action.display_cart()  # Exibe os itens atualmente no carrinho
                 total = action.get_total()  # Calcula o total da compra
-                print(f"Total da compra: {total}")
-                continue_shopping = input("Deseja adicionar mais produtos ao carrinho? (s/n): ")
+                print(f"\033[33mTotal da compra: {total}\033[m")
+                continue_shopping = input("\033[34mDeseja adicionar mais produtos ao carrinho? (s/n): \033[m")
                 if continue_shopping.lower() != 's':
                     break
             except Exception as e:
-                print(f"Ocorreu um erro: {e}")  # Imprime a exceção, se houver
+                print(f"\033[31mOcorreu um erro: {e}\033[m")  # Imprime a exceção, se houver
+                print(self._view.linha())
 
         while True:
-            payment = input("Deseja finalizar a compra? (s/n): ")
+            payment = input("\033[33mDeseja finalizar a compra? (s/n): \033[m")
             if payment.lower() != 's':
                 return  # Se o usuário não quiser finalizar a compra, retorna ao menu inicial
+            else: 
+                print('\033[32mCompra Efetuada com sucesso!\033[m')
+                sleep(1)
             index = 0  # Variável de controle para iterar sobre os itens do carrinho
             cart_item_list = self._cart.get_list()  # Obtém a lista de produtos do carrinho
             for items in self._list_cart:  # Itera sobre os itens armazenados em list_cart
@@ -470,6 +503,7 @@ class Screen():
 
     # Método inicial para exibir o menu principal e capturar a escolha do usuário
     def initial_menu(self) -> None:
+        opcao = ('Adicionar produto já existente ao estoque', 'Adicionar um novo produto ao estoque', 'Alterar preço de um produto ao estoque', 'Retirar um item do estoque', 'Vender um produto')
         """
         Exibe o menu inicial e gerencia a escolha do usuário.
         """
@@ -479,25 +513,30 @@ class Screen():
             if validates:
                 for zero_products in validates:
                     print(zero_products)
-                    time.sleep(1)
+                    sleep(0.5)
             try:
-                print("""Selecione uma das opções
-                    1 - adicionar produto já existente ao estoque
-                    2 - adicionar um novo produto ao estoque
-                    3 - alterar preço de um produto ao estoque
-                    4 - retirar um item do estoque
-                    5 - vender um produto
-                    """)
-                self._initial_choice = int(input("Digite aqui sua opção: "))
+                sleep(1)
+                os.system("cls")
+                self._view.cabecalho('\033[34mMENU PRINCIPAL\033[m')
+                c = 1
+                for item in opcao:
+                    print(f'''\033[32m{c} - \033[34m{item}\033[m''')
+                    c += 1
+                print(self._view.linha())
+                self._initial_choice = int(input("\033[33mDigite aqui sua opção: \033[m"))
+                os.system("cls")
                 if self._initial_choice not in [1, 2, 3, 4, 5]:
-                    raise InvalidChoice("Opção inválida! Tente novamente!")
-                
+                    print("\033[31mOpção inválida! Tente novamente!\033[m")
+                    sleep(1)
+                    return
                 if self._initial_choice == 5:
                     self._handle_cart()
                 
                 else:
                     self._product_menu()
-
                 return
+            
             except (InvalidChoice, ValueError):
-                raise InvalidChoice("Opção inválida! Tente novamente!")
+                print("\033[31mOpção inválida! Tente novamente!\033[m]")
+                sleep(1)
+                return
